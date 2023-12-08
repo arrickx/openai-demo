23f36b262496caa78a8b9b6c374a824b89ed0075
Richter Lau <ar.rick@hotmail.com>
2023-12-07 19:36:55 -0500

    updating chat feature

--- chat.js
+++ chat.js
@@ -1,27 +1,34 @@
+// Importing necessary modules
 import 'dotenv/config'
 import readline from 'node:readline'
 import OpenAI from 'openai'
 
+// Initializing OpenAI with API key
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
+// Creating readline interface for user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
 
+// Function to generate new message using OpenAI
 const newMessage = async (history, message) => {
   const chatCompletion = await openai.chat.completions.create({
     messages: [...history, message],
     model: 'gpt-3.5-turbo',
+    temperature: 0.8,
   })
 
   return chatCompletion.choices[0].message
 }
 
+// Function to format user input into message format
 const formatMessage = (userInput) => ({ role: 'user', content: userInput })
 
+// Main chat function
 const chat = () => {
   const history = [
     {
@@ -49,5 +56,6 @@ const chat = () => {
   start()
 }
 
+// Starting the chatbot
 console.log("Chatbot initialized. Type 'exit' to end the chat.")
 chat()
